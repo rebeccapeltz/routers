d163576339605f382ee46cf1566b29b89e22d527
Rebecca Peltz <rebeccapeltz@gmail.com>
2016-07-03 17:22:27 -0700

    fix test

--- test/router_test.js
+++ test/router_test.js
@@ -3,18 +3,29 @@ const angular = require('angular');
 
 require('angular-mocks');
 require(__dirname + '/../app/js/client.js');
+// let ListController;
+// let $httpBackend;
+describe('crud app tests', () => {
+  beforeEach(() => {
+    angular.mock.module('crudApp');
+    // angular.mock.inject(function($controller, _$httpBackend_) {
+    //   ListController = new $controller('ListController');
+    //   $httpBackend = _$httpBackend_;
+    // });
+  });
 
-it('should map routes to controllers', function() {
-  module('crudApp');
+  it('should map routes to controllers', function() {
+    module('crudApp');
 
-  inject(function($route) {
+    inject(function($route) {
 
-    expect($route.routes['/list'].controller).toBe('ListController');
-    expect($route.routes['/list'].templateUrl).
-    toEqual('/templates/partials/ListView.html');
+      expect($route.routes['/list'].controller).toBe('ListController');
+      expect($route.routes['/list'].templateUrl).
+      toEqual('/templates/partials/ListView.html');
 
 
-    // otherwise redirect to
-    expect($route.routes[null].redirectTo).toEqual('/')
+      // otherwise redirect to
+      expect($route.routes[null].redirectTo).toEqual('/');
+    });
   });
 });
